8c12456ace4a42cc8b9b8bd40d1af65627643049
avisikta19 <47344480+avisikta19@users.noreply.github.com>
2020-10-23 15:36:54 +0530

    Add files via upload

---
+++ Write a function.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    leap = False
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return leap
+        else:
+            return True
+    else:
+        return leap
+
+year = int(input())
+print(is_leap(year))
