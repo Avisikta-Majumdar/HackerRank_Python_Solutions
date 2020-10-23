bc3fbb997219e519d1888f925792796de71dc718
avisikta19 <47344480+avisikta19@users.noreply.github.com>
2020-10-23 15:21:15 +0530

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    addition = a+b
+    subtraction = a-b
+    multiplication = a*b
+    print(addition)
+    print(subtraction)
+    print(multiplication)
