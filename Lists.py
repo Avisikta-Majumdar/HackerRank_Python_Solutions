683cc100703096167a7770a1970252b44e1deac0
avisikta19 <47344480+avisikta19@users.noreply.github.com>
2020-10-27 23:47:59 +0530

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -7,18 +7,19 @@ if __name__ == '__main__':
         if mn[0]=="insert":
             a , b = map(int , (mn[1],mn[2]))
             lst.insert(a,b)
-
-
             
         elif mn[0]=="print":
             print(lst)
+            
         elif mn[0]=="remove":
                 lst.remove(int(mn[1]))
 
         elif mn[0]=="append":
             lst.append(int(mn[1]))
+            
         elif mn[0]=="sort":
             lst.sort()
+        
         elif mn[0]=="pop":
             
             lst.pop()
